4efcdd9fc8f48df77da32f1f53f3ad2326390959
sualeh-farooq <sufiyanfarooq047@gmail.com>
2024-04-24 17:33:04 +0500

    check

--- app.js
+++ app.js
@@ -1,25 +1,57 @@
-let input = document.getElementById('inp')
-let text = document.querySelector(".text")
+let text = document.querySelector(".text");
+let input = document.getElementById('inp');
+var existingTodos = JSON.parse(localStorage.getItem('todos')) || [];
 
-function Add(){
-    text.style.color = "black"
-    if (input.value === ""){
+function renderTodos() {
+    text.innerHTML = "";
+    
+    for(var i = 0; i < existingTodos.length; i++) {
+        text.innerHTML += `
+            <li>
+                ${existingTodos[i].todoItem}
+             
+                <button onclick="deleteTodo(${i})" class="button">
+  <svg viewBox="0 0 448 512" class="svgIcon"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"></path></svg>
+</button>
+            </li>
+        `;
+    }
+}
+
+function Add() {
+    text.style.color = "black";
+    if (input.value === "") {
         Swal.fire({
             icon: "error",
             title: "Please Enter a Task",
             footer: '<a href="#">Why do I have this issue?</a>'
-          });
-    }else{
-        let newEle = document.createElement("ul");
-        newEle.innerHTML =`
-        ${input.value} <i  class="fa-regular fa-circle-xmark" style="cursor: pointer;"></i>
-        `
-         text.appendChild(newEle)
+        });
+    } else {
+        var newTodoItem = input.value;
         input.value = "";
-        newEle.querySelector('i') .addEventListener("click",remove);
-        function remove(){
-            newEle.remove()
-        }
-   
+        existingTodos.push({
+            todoItem: newTodoItem,
+        });
+        localStorage.setItem('todos', JSON.stringify(existingTodos));
+        renderTodos(); // Render the updated list
     }
 }
+
+function deleteTodo(index) {
+    existingTodos.splice(index, 1);
+    localStorage.setItem('todos', JSON.stringify(existingTodos));
+    renderTodos(); // Render the updated list after deletion
+}
+
+// Initial rendering of todos
+renderTodos();
+
+
+// function saveData(){
+//     localStorage.setItem("data",text.innerHTML)
+// }
+
+// function showTask(){
+//     text.innerHTML = localStorage.getItem("data")
+// }
+// showTask() 
